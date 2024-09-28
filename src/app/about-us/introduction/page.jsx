f3313c4e32e011cc01a5a3bc14502c53079b22f6
shubham <shubhamrathourvlog>
2024-09-28 14:58:10 +0530

    design issues fixed

--- src/app/about-us/introduction/page.jsx
+++ src/app/about-us/introduction/page.jsx
@@ -6,57 +6,91 @@ import { Col, Container, Row } from 'react-bootstrap'
 function Introduction() {
   return (
     <div>
-        <Container>
-            <SectionHeading
+    <section className="aboutPageSec secPadding">
+      <Container>
+        <SectionHeading
             tagline={AboutLifeCoachHeading}
-            color="var(--mainYellow)"
-            fontSize={40}
-            fontWeight={600}
-          />
-          <Row>
-            <Col>
-            <div className='about-text'>
-                <p>Leading executive life coach of India, with an 15 years of experience in Life Coaching, Leadership & Self-mastery coaching and Group Facilitation.</p>
-                <p>She's worked as a  Senior Gynaecologist in London, before pursuing her calling to become a  Executive Life coach for Leaders and CEOs.</p>
-                <p>She's passionate about helping people see the greatness within and tap into their highest potential via a shift in their consciousness.</p>
-                <p>She believes in her clients 100% and help them thrive in every area of Business & life.</p>
-            </div>
-            </Col>
-            <Col>
-            <div>
-                <img src='/images/Saloni.png' />
-            </div>
-            </Col>
-          </Row>
+          color="#01464B"
+          fontSize={40}
+          fontWeight={600}
+        />
+        <div className="about-me-section">
+         <Row className="mt-5">
+           <Col md={6}>
+            <div className="aboutMeLeft">
+            <p>
+              Leading executive life coach of India, with an 15 years of
+              experience in Life Coaching, Leadership & Self-mastery coaching
+              and Group Facilitation.
+            </p>
 
+            <p>
+              She's worked as a Senior Gynaecologist in London, before pursuing
+              her calling to become a Executive Life coach for Leaders and CEOs.
+            </p>
+
+            <p>
+              She's passionate about helping people see the greatness within and
+              tap into their highest potential via a shift in their
+              consciousness.
+            </p>
+
+            <p>
+              She believes in her clients 100% and help them thrive in every
+              area of Business & life.
+            </p>
+       
+            </div>
+            </Col>
+            <Col md={6}>
+            <div className="aboutMeImg text-center">
+              <img src="/images/Saloni.png" alt="Dr. Saloni Singh" />
+            </div>
+           </Col> 
+          </Row>
+        </div>
+      </Container>
+    </section>
+    <section className="whoAm_I secPadding">
+       <div className="container">
           <SectionHeading
-            tagline={whoAmIHeading}
-            color="var(--mainYellow)"
+            tagline="Who Am I?"
+            color="#01464B"
             fontSize={40}
             fontWeight={600}
           />
           <div>
+            <p>A Coach. Friend. Spiritual Growth Catalyst.</p>
+
             <p>
-            A Coach. Friend. Spiritual Growth Catalyst.
+              I coach senior leaders, CEOs, entrepreneurs and coaches to help
+              them connect with themselves at a deeper level and become their
+              best version. I am a mom of two teenagers, an author, blogger,
+              dancer and podcaster. A curious explorer of the miracle called
+              life.
             </p>
+
+            <p>But above all this… I am a space holder. I am pure presence.</p>
+
             <p>
-            I coach senior leaders, CEOs, entrepreneurs and coaches to help them connect with themselves at a deeper level and become their best version. I am a mom of two teenagers, an author, blogger, dancer and podcaster. A curious explorer of the miracle called life.
+              I work as a catalyst to help you connect with your highest
+              potential, with deep inner peace, magic and the source within. My
+              deep connection with myself and the universe is my life's work and
+              that's what I bring to everyone I work with. Coaching with me is a
+              deep dive into knowing yourself, discovering and sometimes
+              reinventing yourself.
             </p>
+            <p>I am a sacred space of listening with no judgement.</p>
             <p>
-            But above all this… I am a space holder. I am pure presence.
-            </p>
-            <p>
-            I work as a catalyst to help you connect with your highest potential, with deep inner peace, magic and the source within. My deep connection with myself and the universe is my life's work and that's what I bring to everyone I work with. Coaching with me is a deep dive into knowing yourself, discovering and sometimes reinventing yourself.
-            </p>
-            <p>
-            I am a sacred space of  listening with no judgement.
-            </p>
-            <p>
-            In this space of listening, love & support, you grow and evolve beyond you’ve ever imagined. You start to trust yourself and the universe even more. Miracles happen in your life.
+              In this space of listening, love & support, you grow and evolve
+              beyond you've ever imagined. You start to trust yourself and the
+              universe even more. Miracles happen in your life.
             </p>
           </div>
-        </Container>
+        </div>
+    </section>
     </div>
+
   )
 }
 
--- src/app/about-us/life-journey/page.jsx
+++ src/app/about-us/life-journey/page.jsx
@@ -4,7 +4,7 @@ import SectionHeading from '@/components/SeactionHeading/SectionHeading'
 
 function Lifejourney() {
   return (
-    <div>
+    <div className='lifeJourney secPadding'>
         <SectionHeading
             tagline="Life Journey"
             color="var(--mainYellow)"
@@ -12,36 +12,56 @@ function Lifejourney() {
             fontWeight={600}
             />
         <Container>
-        <Row>
-            <Col>
+    <div className='mt-5'>
+     <div className='lifeJourneyCard bgLightYellow'>
+        <Row className='align-items-center'>
+            <Col md={6} className='order-2 order-md-1'>
             <div className='journey-text'>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo eius labore, in culpa pariatur incidunt laboriosam debitis dolorum consequuntur porro deleniti sit accusamus, quis doloribus assumenda eaque esse? Distinctio, libero?
+             <h4 className='mb3'>Lorem ipsum, dolor sit amet</h4>
+            <p> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo eius labore, in culpa pariatur incidunt laboriosam debitis dolorum consequuntur porro deleniti sit
+            accusamus, quis doloribus assumenda eaque esse? Distinctio, libero?</p>
             </div>
             </Col>
-            <Col>
-            <img src='/images/Saloni.png' />
-            </Col>
-        </Row>
-        <Row>
-            <Col>
-            <img src='/images/Saloni.png' />
-            </Col>
-            <Col>
-            <div className='journey-text'>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo eius labore, in culpa pariatur incidunt laboriosam debitis dolorum consequuntur porro deleniti sit accusamus, quis doloribus assumenda eaque esse? Distinctio, libero?
+            <Col md={6} className='order-1 order-md-2'>
+            <div className='lifeCardImg'>
+               <img src='/images/Saloni.png' alt='saloni' />
             </div>
             </Col>
         </Row>
-        <Row>
-            <Col>
-            <div className='journey-text'>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo eius labore, in culpa pariatur incidunt laboriosam debitis dolorum consequuntur porro deleniti sit accusamus, quis doloribus assumenda eaque esse? Distinctio, libero?
+        </div>
+        <div className='lifeJourneyCard'>
+        <Row className='align-items-center'>
+            <Col md={6}>
+            <div className='lifeCardImg'>
+               <img src='/images/Saloni.png' alt='saloni's />
             </div>
             </Col>
-            <Col>
-            <img src='/images/Saloni.png' />
+            <Col md={6}>
+            <div className='journey-text'>
+            <h4 className='mb3'>Lorem ipsum, dolor sit amet</h4>
+            <p> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo eius labore, in culpa pariatur incidunt laboriosam debitis dolorum consequuntur porro deleniti sit
+            accusamus, quis doloribus assumenda eaque esse? Distinctio, libero?</p>
+            </div>
             </Col>
         </Row>
+        </div>
+        <div className='lifeJourneyCard bgLightYellow'>
+        <Row className='align-items-center'>
+            <Col md={6}  className='order-2 order-md-1'>
+            <div className='journey-text'>
+            <h4 className='mb3'>Lorem ipsum, dolor sit amet</h4>
+               <p> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo eius labore, in culpa pariatur incidunt laboriosam debitis dolorum consequuntur porro deleniti sit
+                 accusamus, quis doloribus assumenda eaque esse? Distinctio, libero?</p>
+            </div>
+            </Col>
+            <Col md={6} className='order-1 order-md-2'>
+             <div className='lifeCardImg'>
+               <img src='/images/Saloni.png' alt='saloni' />
+             </div>
+            </Col>
+        </Row>
+        </div>
+        </div>
         </Container>
     </div>
   )
