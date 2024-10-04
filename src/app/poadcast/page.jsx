72db74e8ebf034e6b93c13f88b5708b1ee4b0b7d
shubham <shubham.kumar@appsmartz.com>
2024-10-04 13:23:04 +0530

    poadcast page design

--- src/app/poadcast/page.jsx
+++ src/app/poadcast/page.jsx
@@ -11,40 +11,40 @@ function Poadcast() {
                     <Row className='align-items-center'>
                         <Col md={6}>
                          <div className='poadCastLeft'>
-                            <h1>Listen to what you LOVE</h1>
-                            <div className='d-flex align-items-center'>
+                            <h1>Listen to what you <span>LOVE</span></h1>
+                            <div className='d-flex align-items-center mt-4'>
                             <h2>WHEREVER</h2>
-                            <div className='ms-3'>
+                            <div className='youAreTxt ms-3'>
                                 <p className='mb-1'>you are,</p>
-                                <p className='mb-1'>you are,</p>
+                                <p className='mb-1'>you go!</p>
                             </div>
                             </div>
                             <div className='storelinks mt-4'>
                                 <Col md={8}>
                                 <Row>
                                 <Col md={6}>
-                                  <div className='text-center'>
+                                  <div className='text-center mb-2'>
                                     <a href='#'>
                                         <img src="/images/playStor.png" alt='playStor'/>
                                     </a>
                                     </div>
                                 </Col>
                                 <Col md={6}>
-                                <div className='text-center'>
+                                <div className='text-center mb-2'>
                                     <a href='#'>
                                         <img src="/images/appStore_Vec.png" alt='appStore'/>
                                     </a>
                                 </div>
                                 </Col>
                                 <Col md={6}>
-                                <div className='text-center'>
+                                <div className='text-center mb-2'>
                                     <a href='#'>
                                         <img src="/images/huawei_vec.png" alt='huawei_vec'/>
                                     </a>
                                 </div>
                                 </Col>
                                 <Col md={6}>
-                                <div className='text-center'>
+                                <div className='text-center mb-2'>
                                     <a href='#'>
                                         <img src="/images/samsung-store.png" alt='samsung'/>
                                     </a>
